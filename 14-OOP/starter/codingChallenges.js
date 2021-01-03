1f434fec46e82f0fa8bcce2e9e2419c054781c7d
Esteban Duran <esteban321duran@gmail.com>
2021-01-02 22:35:04 -0300

    section 14

---
+++ 14-OOP/starter/codingChallenges.js
@@ -0,0 +1,122 @@
+//////////////////////////////////////////////
+// challenge 1
+//////////////////////////////////////////////
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`This ${this.make} is going at ${this.speed} km/h`);
+// };
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`This ${this.make} is going at ${this.speed} km/h`);
+// };
+
+// const car1 = new Car('BMW', 100);
+// const car2 = new Car('Honda', 120);
+// const maxSpeed = 200;
+// const minSpeed = 80;
+
+// const speedUp = function (car) {
+//   car.accelerate();
+//   if (car.speed === maxSpeed) {
+//     clearInterval(accelerationTimer);
+//   }
+// };
+// const speedDown = function (car) {
+//   car.brake();
+//   if (car.speed === minSpeed) {
+//     clearInterval(brakingTimer);
+//   }
+// };
+// const accelerationTimer = setInterval(speedUp, 1000, car1);
+// const brakingTimer = setInterval(speedDown, 1000, car2);
+
+//////////////////////////////////////////////
+// challenge 2
+//////////////////////////////////////////////
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`This ${this.make} is going at ${this.speed} km/h`);
+// };
+// Car.prototype.brake = function
+
+// const kmToMiPerHour = 1.6;
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`This ${this.make} is going at ${this.speed} km/h`);
+//   }
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`This ${this.make} is going at ${this.speed} km/h`);
+//   }
+//   get speedUS() {
+//     return this.speed / kmToMiPerHour;
+//   }
+//   set speedUS(speed) {
+//     this.speed = speed * kmToMiPerHour;
+//   }
+// }
+
+// const car1 = new Car('Nissan', 100);
+// car1.speedUS = 100;
+// console.log(car1.speed);
+// console.log(car1.speedUS);
+// car1.accelerate();
+// console.log(car1.speedUS);
+
+//////////////////////////////////////////////
+// challenge 3
+//////////////////////////////////////////////
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`This ${this.make} is going at ${this.speed} km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`This ${this.make} is going at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+
+EV.prototype.chargeTo = function (amount) {
+  this.charge = amount;
+};
+
+//override Car.accelerate()
+EV.prototype.accelerate = function () {
+  if (this.charge > 0) {
+    this.speed += 20;
+    this.charge -= 1;
+    console.log(
+      `This ${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}%`
+    );
+  } else console.log(`This ${this.make} depleted it's battery.`);
+};
+
+const ev1 = new EV('Tesla', 120, 3);
+
+const speedUp = function (car) {
+  if (car.charge <= 0) clearInterval(accelTimer);
+  car.accelerate();
+};
+
+const accelTimer = setInterval(speedUp, 500, ev1);
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,211 @@
 'use strict';
+
+// ****************************************************
+// Constructor functions and the new operator
+// ****************************************************
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// const calcAge = function (currentYear) {
+//   return currentYear - this.birthYear;
+// };
+
+// const p1 = new Person('Esteban', 2000);
+// const p2 = new Person('Julie', 1995);
+// const currentYear = 2020;
+
+// console.log(Person.prototype);
+// console.log(p1);
+// console.log(p2);
+
+// ****************************************************
+// prototypes
+// ****************************************************
+// Person.prototype.calcAge = calcAge;
+// Person.prototype.species = 'Homo Sapiens';
+
+// const p3 = new Person('Cecilia', 2005);
+
+// console.log(
+//   `The ${p3.species}, ${p3.firstName} is ${p3.calcAge(2021)} years old`
+// );
+// console.log(p3.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(p3));
+
+// console.log(p3.hasOwnProperty('firstName'));
+// console.log(p3.hasOwnProperty('species'));
+
+// ****************************************************
+// prototypal inheritance
+// ****************************************************
+// console.log(p1.__proto__);
+// console.log(p1.__proto__.__proto__);
+// console.log(p1.__proto__.__proto__.__proto__);
+
+// the calling of mehotds like hasOwnProperty, which are defined on the Object() constructor, work thanks to the prototype chain
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [1, 2, 3, 1, 2, 3, 5, 1, 2, 3];
+
+// console.log(arr.__proto__ === Array.prototype);
+// console.log(arr.__proto__.__proto__);
+
+// extending arrays functionality even further by adding methods to the Array prototype
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+// however extending the functionality of built-in objects is a BAD practice
+
+// const h1 = document.querySelector('h1');
+
+// ****************************************************
+// ES6 classes
+// ****************************************************
+// class Person {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
+//   greet() {
+//     console.log(`${this.firstName} says Hi!`);
+//   }
+//   calcAge(currentYear) {
+//     console.log(
+//       `${this.firstName} is ${currentYear - this.birthYear} years old`
+//     );
+//   }
+// }
+// class Label {
+//   constructor(msg) {
+//     this.msg = msg;
+//     this.element = document.createElement('p');
+//     this.element.textContent = this.msg;
+//   }
+// }
+
+// const p1 = new Person('Jessica', 1993);
+
+// console.log(p1);
+// p1.greet();
+// p1.calcAge(2021);
+// console.log(p1.__proto__ === Person.prototype);
+
+// const el = new Label('Change the world. My final message.');
+// document.body.append(el.element);
+// console.log(el);
+
+// ****************************************************
+// static methods
+// ****************************************************
+// const Block = function (type, breakable, dropItem, dropAmount = 1) {
+//   this.type = type;
+//   this.breakable = breakable;
+//   this.dropItem = dropItem;
+//   this.dropAmount = dropAmount;
+// };
+// const mine = function () {
+//   if (this.breakable)
+//     console.log(
+//       `You mine the ${this.type}. You got ${this.dropAmount} ${this.dropItem}`
+//     );
+//   else console.log('This block is unbreakable');
+// };
+// const warn = function () {
+//   console.log(`Going alone at night is very dangerous.`);
+// };
+// Block.prototype.mine = mine;
+// Block.warn = warn;
+// Block.entity = function () {
+//   console.log(this);
+// };
+// const b1 = new Block('iron', true, 'iron ore');
+// console.log(b1);
+// b1.mine();
+// Block.warn();
+// // b1.warn();
+// Block.entity();
+
+// class Entity {
+//   constructor(entityName, uuid) {
+//     this.entityName = entityName;
+//     this.uuid = uuid;
+//   }
+
+//   static killAll() {
+//     console.log('All instances were killed');
+//   }
+//   toString() {
+//     return `The entity ${this.uuid}, is a ${this.entityName}`;
+//   }
+// }
+// const creeper = new Entity('Creeper', 192948);
+
+// Entity.killAll();
+// console.log(creeper.toString());
+
+// ****************************************************
+// Object.crete()
+// ****************************************************
+
+// const PersonProto = {
+//   calcAge(currentYear) {
+//     console.log(
+//       `${this.firstName} is ${currentYear - this.birthYear} years old.`
+//     );
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+// steven.firstName = 'Steven';
+// steven.birthYear = 1999;
+
+// steven.calcAge(2021);
+// console.log(steven.__proto__);
+
+// ****************************************************
+// inheritance between "classes"
+// ****************************************************
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+const Approved = function (firstName, birthYear, course, grade) {
+  Student.call(this, firstName, birthYear, course);
+  this.grade = grade;
+};
+
+//link prototypes. Prototypal inheritance/delegation is achieved
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+Approved.prototype = Object.create(Student.prototype);
+Approved.prototype.constructor = Approved;
+
+const mike = new Student('Mike', 2005, 'Computer Science');
+
+Person.prototype.calcAge = function (currentYear) {
+  return currentYear - this.birthYear;
+};
+Student.prototype.introduce = function () {
+  console.log(
+    `My name is ${this.firstName}. I'm ${this.calcAge(2021)} and I study ${
+      this.course
+    }.`
+  );
+};
+
+// mike.introduce();
+// console.dir(Student.prototype.constructor);
+const sussie = new Approved('Sussie', 2002, 'Accounting', 8.9);
+
+//Person defines calcAge()
+//Student defines introduce()
+//an object created from the Student constructor has access to the methods in the prototype property of the Student constructor.
